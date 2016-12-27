48ff4db9731245d1feb88c290302154c6e7253cb
Chiptopher <chrisboyerdev@gmail.com>
2016-12-26 23:19:57 -0500

    Updated Summart
    Incorrect summary in summary block.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 setup(
     name='scalepy',
     description='Provies renaming utility for Nikon output files.',
-    version='0.1',
+    version='0.2',
     url='https://github.com/ChrisoftheBoyerClan/scale.py',
     author='Christopher Boyer',
     author_email='ChrisoftheBoyerClan@gmail.com',
-    entry_points={ 'console_scripts': [ 'scalepy=scale.scale:main', ], },
+    entry_points={ 'console_scripts': [ 'scalepy=scalepy.scale:main', ], },
     packages=['scalepy'],
     license='MIT',
     keywords='scale image resize'
